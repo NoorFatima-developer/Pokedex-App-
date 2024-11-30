3b15c13a9dead7dfb3ad82b007bfcfc380cadc9d
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-01 03:49:06 +0500

    code is added

--- pokemon.js
+++ pokemon.js
@@ -1,2 +1,6 @@
 const MAX_POKEMON = 151;
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
 const listWrapper = document.querySelector(".list-wrapper");
+const notFouneMessage = document.querySelector(".not-found-message");
