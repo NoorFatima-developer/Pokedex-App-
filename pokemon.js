eeb39d37c716b6402b50abba6b3f7531b1356057
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-02 01:23:36 +0500

    code is added

--- pokemon.js
+++ pokemon.js
@@ -50,7 +50,12 @@ function displayPokemons(pokemon) {
     <div class = "number-wrap">
     <p class = "caption-fonts">#${pokemonID}</p>
     </div>
+
     <div class = "img-wrap">
     <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+    </div>
+
+    <div class = "name-wrap">
+    <p class = "body3-fonts">#${pokemonID}</p>
     </div>`
 }
