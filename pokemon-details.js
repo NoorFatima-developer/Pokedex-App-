61e543a9ea2509b95783e80156e8b5140e5001cb
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-02 04:40:41 +0500

    code is added

---
+++ pokemon-details.js
@@ -0,0 +1,6 @@
+let currentPokemon = null;
+
+document.addEventListener("DOMContentLoaded", function() { 
+    const MAX_POKEMON = 151;
+    const pokemonID = new URLSearchParams(window.location.search).get("Id");
+})
