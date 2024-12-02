4a27808b1da98d0a13965bb0df95fc6bc4bc583c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-03 02:31:07 +0500

    code is added

--- search.js
+++ search.js
@@ -1,37 +1,25 @@
 const inputElement = document.querySelector("#search-input");
 const search_icon = document.querySelector("#search-close-icon");
-const sort_wrapper = document.querySelector(".sort-wrapper");
+const sort_wrapper = document.querySelector("#sort-wrapper");
 
-inputElement.addEventListener("input", () => {
-  handleInputChange(inputElement);
+inputElement.addEventListener("input", function(){
+    handleInputChange(inputElement);
 });
-search_icon.addEventListener("click", handleSearchCloseOnClick);
-sort_wrapper.addEventListener("click", handleSortIconOnClick);
+search_icon.addEventListener("click", function(){
+    handleSearchCloseOnClick;
+});
+
+sort_wrapper.addEventListener("click", function(event){
+    handleSortIconOnClick;
+});
+
 
 function handleInputChange(inputElement) {
-  const inputValue = inputElement.value;
+    const inputValue = inputElement.value;
 
-  if (inputValue !== "") {
-    document
-      .querySelector("#search-close-icon")
-      .classList.add("search-close-icon-visible");
-  } else {
-    document
-      .querySelector("#search-close-icon")
-      .classList.remove("search-close-icon-visible");
-  }
-}
-
-function handleSearchCloseOnClick() {
-  document.querySelector("#search-input").value = "";
-  document
-    .querySelector("#search-close-icon")
-    .classList.remove("search-close-icon-visible");
-}
-
-function handleSortIconOnClick() {
-  document
-    .querySelector(".filter-wrapper")
-    .classList.toggle("filter-wrapper-open");
-  document.querySelector("body").classList.toggle("filter-wrapper-overlay");
+    if(inputValue !== ""){
+        document.querySelector("search-close-icon").classList.add("search-close-icon-visible");
+    } else {
+        document.querySelector("search-close-icon").classList.remove("search-close-icon-visible");
+    }
 }
